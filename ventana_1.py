fde85eb0273a1fd535ebb15700bf75601e23f2b4
jade6600 <sortehflorez@colegioguanenta.edu.co>
2025-07-31 15:50:28 -0500

    .

---
+++ ventana_1.py
@@ -0,0 +1,22 @@
+# Se importa la librería tkinter con todas sus funciones.
+from tkinter import *
+
+#--------------------------------------------------------
+# Funciones de la aplicación
+#--------------------------------------------------------
+
+
+#--------------------------------------------------------
+# Ventana Principal de la app
+#--------------------------------------------------------
+
+# Se declara una variable llamada una ventana_principal. que adquiere las características de un objeto de tipo TK(). 
+ventana_principal = Tk()
+
+# Título de la ventana
+ventana_principal.title("Soreth Flórez")
+ventana_principal.geometry("800x500")
+
+# Método principal que despliega la ventana en pantalla.
+ventana_principal.mainloop()
+
